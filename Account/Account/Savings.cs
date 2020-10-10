ebd184884a8eb129503401813c4695000d825282
omnia266 <omniaeid266@gmail.com>
2020-10-10 07:18:42 -0500

    practice

---
+++ Account/Account/Savings.cs
@@ -0,0 +1,20 @@
+﻿namespace Inheritance
+{
+    namespace Inheritance
+    {
+        namespace Inheritance
+        {
+            namespace Inheritance
+            {
+                class Savings : Account
+                {
+                    public Savings(string accountName, decimal balance, float interestRate) : base(accountName, balance, interestRate)
+                    {
+                        //      gift of a $100 for opening an new account
+                        this.Deposit(100);
+                    }
+                }
+            }
+        }
+    }
+}
